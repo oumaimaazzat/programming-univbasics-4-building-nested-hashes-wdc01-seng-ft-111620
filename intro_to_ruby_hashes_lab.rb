826226377c820c9741b6366dd7a86d4089a6205e
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-11 22:54:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,12 +18,12 @@ def monopoly_with_third_tier
     {
       :pieces=> 4,
       :rent_in_dollars=> {}
-        {
+    }
           :one_piece_owned=>25,
           :two_pieces_owned=>50,
           :three_pieces_owned=>100,
           :four_pieces_owned=>200
-        },
+        end 
       :names=>
         {
           :reading_railroad => {},
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
           :b_and_o_railroad => {},
           :shortline_railroad => {}
         }
-    }
+    end 
   }
 end
 
