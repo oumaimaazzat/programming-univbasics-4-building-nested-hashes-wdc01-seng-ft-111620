9092dd41536141eea4c1d3fb89b6af7a33768833
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-11 22:38:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,7 @@ def monopoly_with_fourth_tier
   :railroads=>
   {
     :pieces=>4,
-    :rent_in_dollars=> {}
+    :rent_in_dollars=>{}
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
