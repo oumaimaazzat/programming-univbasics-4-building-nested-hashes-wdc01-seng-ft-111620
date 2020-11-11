0bb67227eb1df210076172520985a6443fb8d6e1
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-11 22:56:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,15 @@
 def base_hash
+  # Remember implicit return! We're returning this new Hash _without_ the
+  # keyword "return." Nice and neat.
 	{}
 	monopoly = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
   monopoly = {
     :railroads => 
     {
@@ -13,29 +19,37 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
   monopoly =  {
     :railroads=>
     {
       :pieces=> 4,
-      :rent_in_dollars=> {}
-    }
-        { :one_piece_owned=>25,
+      :rent_in_dollars=>
+        {
+          :one_piece_owned=>25,
           :two_pieces_owned=>50,
           :three_pieces_owned=>100,
           :four_pieces_owned=>200
-        end 
-  
+        },
+      :names=>
         {
           :reading_railroad => {},
           :pennsylvania_railroad => {},
           :b_and_o_railroad => {},
           :shortline_railroad => {}
         }
-    end 
+    }
   }
 end
 
 def monopoly_with_fourth_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
    monopoly =  {
   :railroads=>
   {
@@ -68,4 +82,5 @@ def monopoly_with_fourth_tier
     }
   }
 }
+
 end
